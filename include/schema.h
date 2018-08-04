7a7b230b0df7913f2ded5bbb5cf1110e58dab05f
AnIcepIg <anicepig@gmail.com>
2018-08-05 00:34:02 +0800

    add basic functions demo project

---
+++ include/schema.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "utility.h"
+#include "bittwiddle.h"
+#include "particular_alloc.h"
+#include "itable.h"
+#include "codeflow.h"
+#include "icstr.h"
+#include "tcontainer.h"
+#include "debug.h"
